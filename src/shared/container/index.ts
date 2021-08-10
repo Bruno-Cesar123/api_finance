8511c2461feac3d292c3f64eb89bf4a5357c7e8b
Bruno Cesar <brunocesar12386@gmail.com>
2021-08-10 14:46:11 -0300

    create and config userTokenRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,8 +9,8 @@ import FinancesRepository from '@modules/finances/infra/typeorm/repositories/Fin
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IFinancesRepository>(
   'FinancesRepository',
@@ -20,4 +20,9 @@ container.registerSingleton<IFinancesRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import financeRouter from '@modules/finances/infra/http/routes/finances.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/finance', financeRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
