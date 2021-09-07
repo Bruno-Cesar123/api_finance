6a4f06e3bf56ec1e7aba3adfa7bd829ea823d7ef
Bruno Cesar <brunocesar12386@gmail.com>
2021-09-06 21:00:48 -0300

    refactor code list

--- src/modules/finances/services/DeleteFinanceService.ts
+++ src/modules/finances/services/DeleteFinanceService.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IFinancesRepository from '../repositories/IFinancesRepository';
 
 interface IRequest {
@@ -13,6 +14,9 @@ class DeleteFinanceService {
   constructor(
     @inject('FinancesRepository')
     private financesRepository: IFinancesRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ id }: IRequest): Promise<void> {
@@ -23,6 +27,8 @@ class DeleteFinanceService {
     }
 
     await this.financesRepository.deleteFinance(finance);
+
+    await this.cacheProvider.invalidatePrefix('finances-list');
   }
 }
 
